4426667b4996ae06a7ab953149634e5d869ddd46
João Victor Kulka <jv.kulka13@gmail.com>
2023-04-12 15:50:37 -0300

    Arrays React
    Realizado segundo exercício para estudo em manipulação de Arrays através de React

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,58 +1,107 @@
 import React from "react";
 
-// 1 EXERCICIO 
+// 1 EXERCICIO
 
 // Mostre os dados da aplicação, como aprensetado no vídeo
 // Não utilize CSS externo, use o style para mudar as cores
 // Se a situação estiver ativa pinte de verde, inativa vermelho
 // Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
 
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
+// const luana = {
+//   cliente: "Luana",
+//   idade: 27,
+//   compras: [
+//     { nome: "Notebook", preco: "R$ 2500" },
+//     { nome: "Geladeira", preco: "R$ 3000" },
+//     { nome: "Smartphone", preco: "R$ 1500" },
+//   ],
+//   ativa: true,
+// };
+
+// const mario = {
+//   cliente: "Mario",
+//   idade: 31,
+//   compras: [
+//     { nome: "Notebook", preco: "R$ 2500" },
+//     { nome: "Geladeira", preco: "R$ 3000" },
+//     { nome: "Smartphone", preco: "R$ 1500" },
+//     { nome: "Guitarra", preco: "R$ 3500" },
+//   ],
+//   ativa: false,
+// };
+
+// const App = () => {
+//   const dados = mario;
+
+//   const total = dados.compras
+//     .map((item) => Number(item.preco.replace("R$ ", "")))
+//     .reduce((a, b) => a + b);
+
+//   return (
+//     <div>
+//       <p>Nome: {dados.cliente}</p>
+//       <p>Idade: {dados.idade}</p>
+//       <p>
+//         Situação:{" "}
+//         <span style={{ color: dados.ativa ? "green" : "red" }}>
+//           {dados.ativa ? "Ativa" : "Inativa"}
+//         </span>
+//       </p>
+//       <p>Total gastos: R$ {total}</p>
+//       {total > 10000 && <p>Você está gastando muito.</p>}
+//     </div>
+//   );
+// };
+
+//------------------------------------------------------------------------//
+
+// 2 EXERCICIO
+
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  const dados = mario;
-
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace("R$ ", "")))
-    .reduce((a, b) => a + b);
-
   return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{" "}
-        <span style={{ color: dados.ativa ? "green" : "red" }}>
-          {dados.ativa ? "Ativa" : "Inativa"}
-        </span>
-      </p>
-      <p>Total gastos: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito.</p>}
-    </div>
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>Preço: {produto.preco}</p>
+            <ul>
+              {produto.cores.map((cor) => (
+                <li key={cor} style={{ backgroundColor: cor, color: "white" }}>
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </section>
   );
 };
 
-export default App;
-
-
 //------------------------------------------------------------------------//
+
+export default App;
