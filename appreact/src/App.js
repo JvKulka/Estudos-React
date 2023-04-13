a94a189654a174b768d5055e96ee382eafcf77e3
João Victor Kulka <jv.kulka13@gmail.com>
2023-04-13 14:56:33 -0300

    Exercício Manipulação de Arrays, componentes e propriedades
    Realizado terceiro exercicio para prática em React, abordando manipulação de Arrays, componentes e propriedades.

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./Header";
+import Home from "./Home";
+import Produtos from "./Produtos";
 
 // 1 EXERCICIO
 
@@ -59,49 +62,74 @@ import React from "react";
 
 // Organize os produtos como mostrado no vídeo
 // Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
+// const produtos = [
+//   {
+//     id: 1,
+//     nome: "Smartphone",
+//     preco: "R$ 2000",
+//     cores: ["#29d8d5", "#252a34", "#fc3766"],
+//   },
+//   {
+//     id: 2,
+//     nome: "Notebook",
+//     preco: "R$ 3000",
+//     cores: ["#ffd045", "#d4394b", "#f37c59"],
+//   },
+//   {
+//     id: 3,
+//     nome: "Tablet",
+//     preco: "R$ 1500",
+//     cores: ["#365069", "#47c1c8", "#f95786"],
+//   },
+// ];
+
+// const App = () => {
+//   return (
+//     <section>
+//       {produtos
+//         .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
+//         .map((produto) => (
+//           <div key={produto.id}>
+//             <h1>{produto.nome}</h1>
+//             <p>Preço: {produto.preco}</p>
+//             <ul>
+//               {produto.cores.map((cor) => (
+//                 <li key={cor} style={{ backgroundColor: cor, color: "white" }}>
+//                   {cor}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         ))}
+//     </section>
+//   );
+// };
+
+//------------------------------------------------------------------------//
+
+// 3 EXERCICIO
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
 
 const App = () => {
+  const { pathname } = window.location;
+  let Pagina;
+
+  if (pathname === "/produtos") {
+    Pagina = Produtos;
+  } else {
+    Pagina = Home;
+  }
+
   return (
     <section>
-      {produtos
-        .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
-        .map((produto) => (
-          <div key={produto.id}>
-            <h1>{produto.nome}</h1>
-            <p>Preço: {produto.preco}</p>
-            <ul>
-              {produto.cores.map((cor) => (
-                <li key={cor} style={{ backgroundColor: cor, color: "white" }}>
-                  {cor}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+      <Header />
+      <Pagina />
     </section>
   );
 };
-
-//------------------------------------------------------------------------//
 
 export default App;
