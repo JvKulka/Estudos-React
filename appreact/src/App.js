7d6ff06f33001eb05d0d48d4fa779f828cf0b088
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-08 18:15:54 -0300

    useFetch
    Realizado a criação de Custom Hooks de um useFetch

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useLocalStorage from "./useLocalStorage";
+import useFetch from "./useFetch";
 
 // 1 EXERCICIO
 
@@ -239,18 +240,41 @@ import useLocalStorage from "./useLocalStorage";
 
 const App = () => {
   const [produto, setProduto] = useLocalStorage("produto", "");
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request(
+        "https://ranekapi.origamid.dev/json/api/produto/"
+      );
+      console.log(response, json);
+    }
+    fetchData();
+  }, [request]);
 
   function handleClick({ target }) {
     setProduto(target.innerText);
   }
 
-  return (
-    <div>
-      <p>Preferido: {produto}</p>
-      <button onClick={handleClick}>notebook</button>
-      <button onClick={handleClick}>smartphone</button>
-    </div>
-  );
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data)
+    return (
+      <div>
+        <p>Preferido: {produto}</p>
+        <button onClick={handleClick} style={{ margin: "1rem" }}>
+          notebook
+        </button>
+        <button onClick={handleClick}>smartphone</button>
+
+        {data.map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
 };
 
 export default App;
