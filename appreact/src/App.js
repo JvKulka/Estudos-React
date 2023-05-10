5166859f07b5a70aa2682f13328be58919425079
João Victor Kulka <jv.kulka13@gmail.com>
2023-05-10 09:59:08 -0300

    Desafio Formulários
    Realizado a criação de um Quiz de perguntas utilizando Forms para fixação do conteúdo.

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Input from "./input";
-import useForm from "./useForm";
+import Radio from "./Radio";
 
 // import useLocalStorage from "./useLocalStorage";
 // import useFetch from "./useFetch";
@@ -384,28 +383,120 @@ import useForm from "./useForm";
 
 //------------------------------------------------------------------------//
 
-const App = () => {
-  const cep = useForm("cep");
+// Criação de components Forms
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (cep.validate()) {
-      console.log("Enviar");
+// const App = () => {
+//   const cep = useForm("cep");
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     if (cep.validate()) {
+//       console.log("Enviar");
+//     } else {
+//       console.log("Não enviar");
+//     }
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <Input
+//         label="CEP"
+//         id="cep"
+//         type="text"
+//         placeholder="00000-000"
+//         {...cep}
+//       />
+//       <button>Enviar</button>
+//     </form>
+//   );
+// };
+
+//------------------------------------------------------------------------//
+
+// Desafio Formulários
+
+const perguntas = [
+  {
+    pergunta: "Qual método é utilizado para criar componentes?",
+    options: [
+      "React.makeComponent()",
+      "React.createComponent()",
+      "React.createElement()",
+    ],
+    resposta: "React.createElement()",
+    id: "p1",
+  },
+  {
+    pergunta: "Como importamos um componente externo?",
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: "p2",
+  },
+  {
+    pergunta: "Qual hook não é nativo?",
+    options: ["useEffect()", "useFetch()", "useCallback()"],
+    resposta: "useFetch()",
+    id: "p3",
+  },
+  {
+    pergunta: "Qual palavra deve ser utilizada para criarmos um hook?",
+    options: ["set", "get", "use"],
+    resposta: "use",
+    id: "p4",
+  },
+];
+
+const App = () => {
+  const [respostas, setRespostas] = React.useState({
+    p1: "",
+    p2: "",
+    p3: "",
+    p4: "",
+  });
+  const [slide, setSlide] = React.useState(0);
+  const [resultado, setResultado] = React.useState(null);
+
+  function handleChange({ target }) {
+    setRespostas({ ...respostas, [target.id]: target.value });
+  }
+
+  function resultadoFinal() {
+    const corretas = perguntas.filter(
+      ({ id, resposta }) => respostas[id] === resposta
+    );
+    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
+  }
+
+  function handleClick() {
+    if (slide < perguntas.length - 1) {
+      setSlide(slide + 1);
     } else {
-      console.log("Não enviar");
+      setSlide(slide + 1);
+      resultadoFinal();
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input
-        label="CEP"
-        id="cep"
-        type="text"
-        placeholder="00000-000"
-        {...cep}
-      />
-      <button>Enviar</button>
+    <form onSubmit={(event) => event.preventDefault()}>
+      {perguntas.map((pergunta, index) => (
+        <Radio
+          active={slide === index}
+          key={pergunta.id}
+          value={respostas[pergunta.id]}
+          onChange={handleChange}
+          {...pergunta}
+        />
+      ))}
+
+      {resultado ? (
+        <p>{resultado}</p>
+      ) : (
+        <button onClick={handleClick}>Próximo</button>
+      )}
     </form>
   );
 };
